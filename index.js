5c1329d287692b34de4625cdb6038ad7cc35cfb4
DALyskov <DALyskov@gmail.com>
2022-03-11 22:35:39 +0300

    удалил папку пакетов

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
 const homeRouter = require('./routs/home');
 const coursesRouter = require('./routs/courses');
 const addRouter = require('./routs/add');
@@ -27,6 +29,18 @@ app.use('/courses', coursesRouter);
 app.use('/add', addRouter);
 app.use('/card', cardRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+async function start() {
+  try {
+    const password = 'PUBM9NWrpj6jQgMx';
+    const url = `mongodb+srv://DALyskov:${password}@cluster0.xu09b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+    await mongoose.connect(url, {useNewUrlParser: true});
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+start();
